97c900956e01595903eec43ce31dd3ab9c7b4eea
agungdhewe <agung_dhewe@yahoo.com>
2024-07-02 10:45:40 +0700

    add menu mobile

--- data/template/eag2/mainmenu-mobile.mjs
+++ data/template/eag2/mainmenu-mobile.mjs
@@ -1,6 +1,7 @@
 const btn_open = document.getElementById("mainmenu-mobile-btn_open")
 const btn_close = document.getElementById("mainmenu-mobile-btn_close")
 const pnl_menu = document.getElementById("mainmenu-mobile")
+const mainmenu = document.querySelector("#mainmenu-mobile nav")
 
 export function Init(opt) {
 
@@ -11,6 +12,25 @@ export function Init(opt) {
 	btn_close.addEventListener('click', () => {
 		btn_close_click()
 	});
+	
+	var acc = mainmenu.getElementsByClassName("accordion");
+	var i;
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			/* Toggle between adding and removing the "active" class,
+			to highlight the button that controls the panel */
+			this.classList.toggle("accordion-active");
+		
+			/* Toggle between hiding and showing the active panel */
+			var panel = this.nextElementSibling;
+			if (panel.style.display === "block") {
+			panel.style.display = "none";
+			} else {
+			panel.style.display = "block";
+			}
+		});
+	}
+	
 
 }
 
